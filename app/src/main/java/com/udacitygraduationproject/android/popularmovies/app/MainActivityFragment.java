b9d44b70d6f6074ba5d2be264bc5effd40323d4b
AlaaMaher <alaamaherm@gmail.com>
2016-01-27 23:26:22 +0200

    ImgeAdapter

--- app/src/main/java/com/udacitygraduationproject/android/popularmovies/app/MainActivityFragment.java
+++ app/src/main/java/com/udacitygraduationproject/android/popularmovies/app/MainActivityFragment.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.util.Log;
+import android.widget.GridView;
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +18,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+                        GridView gridview = (GridView) view.findViewById(R.id.grid_view);
+
+                        if (gridview == null) {
+                        Log.d("MYLOG", "gridview is null");
+                    } else {
+                        Log.d("MYLOG", "gridview is NOT null");
+                    }
+
+                        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+                        return view;
     }
-}
+    }
+

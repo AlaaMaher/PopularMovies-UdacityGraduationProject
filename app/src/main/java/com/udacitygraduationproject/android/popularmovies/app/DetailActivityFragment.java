3078612b31c8ded08c8fb535802cda368e292d64
AlaaMaher <alaamaherm@gmail.com>
2016-01-28 18:28:58 +0200

    Details activity

---
+++ app/src/main/java/com/udacitygraduationproject/android/popularmovies/app/DetailActivityFragment.java
@@ -0,0 +1,22 @@
+package com.udacitygraduationproject.android.popularmovies.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alaa on 28/01/2016.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
